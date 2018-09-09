139a620f0e2c2bdc9a12654adcae87a6296ac3a5
jpenrici <jpenrici@gmail.com>
2018-09-09 10:02:02 -0300

    Update

--- TCC_OpenCV_Cpp/image_tools.hpp
+++ TCC_OpenCV_Cpp/image_tools.hpp
@@ -17,16 +17,16 @@ namespace img_tools {
 	static const std::string WORKSPACE_IMGP = "Workspace/imgp/";
 	static const std::string METHOD = "M0_";
 
-	std::unordered_map<std::string, std::string> map_imgp_default = {
+	static const std::unordered_map<std::string, std::string> map_extension = {
+		{"PNG", "png"}, {"JPG", "jpg"}, {"JPEG", "jpeg"},
+	};
+
+	static const std::unordered_map<std::string, std::string> map_default = {
 		{"ORIGINAL"  , "output/original/"},
 		{"GRAYSCALE" , "output/grayscale/gray-"},
 		{"BINARY"    , "output/binary/bin-"},
 		{"BINARY_INV", "output/binary/bininv-"}
-	};
-
-	std::unordered_map<std::string, std::string> map_extension = {
-		{"PNG", "png"}, {"JPG", "jpg"}, {"JPEG", "jpeg"},
-	};
+	};	
 
 	std::unordered_map<std::string, std::string> map_imgp;
 
@@ -36,15 +36,20 @@ namespace img_tools {
 	std::string method_name(METHOD);
 	std::string imgp_path, path_temp;
 
+	void error(int line, const std::string& msg)
+	{
+		throw std::domain_error("[Line: " + std::to_string(line) + "]: " + msg);
+	}	
+
 	void view_map_load()
 	{
 		std::cout << "Map currently loaded:\n";
-		tools::view_map(map_imgp);
-	}
-
-	std::string get_path_map(const std::string& key)
-	{
-		return tools::find_key(key, map_imgp);
+		try {
+			tools::view_map(map_imgp);
+		}
+		catch(const std::exception& e) {
+			error(__LINE__, "img_tools::view_map_load");
+		}
 	}
 
 	void status_map()
@@ -53,25 +58,35 @@ namespace img_tools {
 		std::cout << "\nIMGP: " << imgp_path
 			<< "\nfilename: " << filename
 			<< "\nname: " << name << '\n';
+	}
+
+	std::string get_path_map(const std::string& key)
+	{
+		return tools::find_key(key, map_imgp);
 	}	
 
 	void validate (const std::string& file_image)
 	{
 		if (!tools::check_extension(tools::upper(extension), map_extension))
-			throw std::domain_error("error:img_tools::validate,"
-			" invalid file_image in.\n");		
+			error(__LINE__, "img_tools::validate, invalid file_image in.\n");		
 	}
 
 	void info_path(const std::string& file_image)
 	{
-		tools::split_path(file_image, info);
+		try {
+			tools::split_path(file_image, info);
+		}
+		catch(const std::exception& e) {
+			error(__LINE__, "img_tools::info_path\n");
+		}
+		
 		directory = info[0];
 		filename = info[1];
 		extension = info[2];
 		name = info[3];		
 	}
 
-	void initialize_imgp(const std::string& file_image)
+	void initialize_imgp_default(const std::string& file_image)
 	{
 		try {
 			info_path(file_image);	
@@ -79,10 +94,10 @@ namespace img_tools {
 			map_imgp.clear();
 
 			imgp_path = workspace_imgp + method_name + name + ".imgp";
-			std::cout << "start: " << imgp_path << "\n";
+			std::cout << "initialize .imgp: " << imgp_path << "\n";
 			tools::save_new_file(imgp_path);
 
-			for (auto it: map_imgp_default) {
+			for (auto it: map_default) {
 				path_temp = std::string(it.first) + ":" + workspace + 
 					std::string(it.second) + filename + '\n';
 				map_imgp.insert(std::make_pair<std::string, std::string>
@@ -90,27 +105,27 @@ namespace img_tools {
 					std::string(it.second) + filename));
 				tools::write(path_temp, imgp_path);
 			}
-			std::cout << "img_tools::initialize_imgp ... ok" << '\n';
+			std::cout << "img_tools::initialize_imgp_default ... ok" << '\n';
 		}
 		catch(const std::exception& e) {
-			std::cout << e.what() << '\n';
+			error(__LINE__, "img_tools::initialize_imgp_default");
 		}		
 	}
 
 	void create_imgp(const std::string& file_image)
 	{
-
-		if (map_imgp.empty()) {
-			std::cout << "map currently empty!\n";
-			return;
-		}
-
 		try {
+
+			if (map_imgp.empty()) {
+				std::cout << "map currently empty!\n";
+				return;
+			}		
+				
 			info_path(file_image);	
 			validate(file_image);			
 
 			imgp_path = workspace_imgp + method_name + name + ".imgp";
-			std::cout << "start: " << imgp_path << "\n";
+			std::cout << "create .imgp: " << imgp_path << "\n";
 			tools::save_new_file(imgp_path);
 
 			for (auto it: map_imgp) {
@@ -124,22 +139,23 @@ namespace img_tools {
 			std::cout << "img_tools::create_imgp ... ok" << '\n';
 		}
 		catch(const std::exception& e) {
-			std::cout << e.what() << '\n';
+			error(__LINE__, "img_tools::create_imgp");
 		}		
 	}	
 
 	void update_imgp(const std::string& file_imgp)
 	{
-		if (!tools::check_filename_extension(file_imgp, "imgp"))
-			throw std::domain_error("error:img_tools::update_imgp,"
-			" invalid file_image in.\n");
-
-		if (map_imgp.empty()) {
-			std::cout << "map currently empty!\n";
-			return;
-		}
-
 		try {
+
+			if (!tools::check_filename_extension(file_imgp, "imgp"))
+				error(__LINE__, 
+					"img_tools::update_imgp, invalid file_image in.\n");
+
+			if (map_imgp.empty()) {
+				std::cout << "map currently empty!\n";
+				return;
+			}
+
 			std::string file_temp =  tools::get_filename(file_imgp);
 			imgp_path = file_imgp;
 			tools::save_new_file(imgp_path);
@@ -152,7 +168,7 @@ namespace img_tools {
 			std::cout << "img_tools::update ... ok" << '\n';
 		}
 		catch(const std::exception& e) {
-			std::cout << e.what() << '\n';
+			error(__LINE__, "img_tools::update_imgp");
 		}
 	}
 
@@ -173,9 +189,8 @@ namespace img_tools {
 			info_path(file_temp);
 		}
 		catch(const std::exception& e) {
-			std::cout << e.what() << '\n';
+			error(__LINE__, "img_tools::recover_imgp");
 		}
-
 	}
 };
 
