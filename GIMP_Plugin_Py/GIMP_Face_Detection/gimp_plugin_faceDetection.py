3af2b4c4462bf08b59a0ba676e98d1ec70a70a06
jpenrici <jpenrici@gmail.com>
2020-08-04 17:14:37 -0300

    Continuam as experiências com GIMP Plugin.

--- GIMP_Plugin_Py/GIMP_Face_Detection/gimp_plugin_faceDetection.py
+++ GIMP_Plugin_Py/GIMP_Face_Detection/gimp_plugin_faceDetection.py
@@ -2,7 +2,15 @@
 
 '''
     Face Detection with OpenCV
-    ==============
+    =========
+
+    pdb.python_fu_faceDetection(img, drw, option)
+
+    Analysis options for detected faces:
+    1) option="detection" : Only mark with a rectangular border.
+    2) option="selection" : Only select for later copy and paste.
+
+    Note:
     Opencv-python and Numpy requirements.
 '''
 
@@ -28,7 +36,8 @@ ENV = PATH + "/pyenv/lib/python2.7"
 CASCADE_PATH = PATH + "/haarcascade_frontalface_default.xml"
 
 # Log
-log = "\nGIMP: " + str(datetime.datetime.now().strftime("%b %d %Y %H:%M:%S"))
+now = datetime.datetime.now()
+log = "\nGIMP: " + str(now.strftime("%b %d %Y %H:%M:%S"))
 log += "\nPlug-in: " + LABEL + '\n'
 logError = "Unexpected error: "
 
@@ -36,7 +45,8 @@ logError = "Unexpected error: "
 if not os.path.isdir(ENV):
     logError += "pyenv/lib/python2.7 ... not found\n"
 else:
-    sys.path.extend([ENV, ENV + "/site-packages", ENV + "/site-packages/setuptools"])
+    sys.path.extend([ENV, ENV + "/site-packages",
+                     ENV + "/site-packages/setuptools"])
 
 # Dependências
 dependencies = True
@@ -59,8 +69,9 @@ if not os.path.isfile(CASCADE_PATH):
 else:
     log += CASCADE_PATH + " ... ok\n"
 
-log += logError + "...\n"
+log += logError + " ...\n"
 # print(log)
+
 
 def message(msg):
     pdb.gimp_message(msg)
@@ -69,13 +80,13 @@ def message(msg):
 def pxRgnToNumpy(layer):
     rgn = layer.get_pixel_rgn(0, 0, layer.width, layer.height, False, False)
     values = array("B", rgn[0:layer.width, 0:layer.height])  # uchar -> int
-    return np.array(values, dtype=np.uint8).reshape(layer.height, layer.width,
-                   layer.bpp)
+    npArray = np.array(values, dtype=np.uint8)
+    return npArray.reshape(layer.height, layer.width, layer.bpp)
 
 
 def createNewLayer(img, name, npArray):
-    newLayer = gimp.Layer(img, name, img.width, img.height, img.active_layer.type,
-                    100, NORMAL_MODE)
+    newLayer = gimp.Layer(img, name, img.width, img.height,
+                          img.active_layer.type, 100, NORMAL_MODE)
     rgn = newLayer.get_pixel_rgn(0, 0, newLayer.width, newLayer.height, True)
     rgn[:, :] = np.uint8(npArray).tobytes()  # gimp.PixelRgn
     img.add_layer(newLayer, lastLayer(img))
@@ -91,7 +102,7 @@ def lastLayer(img):
 
 
 def saveLog(text):
-    filename = "LogGimpPlugin.txt"
+    filename = "LogGimpPlugin" + str(now.strftime("%b%d%Y_%H%M%S")) + ".txt"
     try:
         filename = open(filename, "w")
         filename.write(text)
@@ -100,7 +111,7 @@ def saveLog(text):
         pass
 
 
-def faceDetection(img, layer):
+def faceDetection(img, layer, option):
 
     global log
 
@@ -111,9 +122,11 @@ def faceDetection(img, layer):
         return
 
     inform = "Processing " + img.name + " ..."
-    log += inform + '\n'
     gimp.progress_init(inform)
     pdb.gimp_image_undo_group_start(img)
+
+    log += inform + '\n'
+    log += "Face " + option + " ...\n"
 
     try:
         # OpenCV
@@ -124,27 +137,37 @@ def faceDetection(img, layer):
         clf = cv.CascadeClassifier(CASCADE_PATH)
         faces = clf.detectMultiScale(img_gray, 1.3, 5)
 
-        hits = 0
-        for (x, y, w, h) in faces:
-            hits += 1
-            img_copy = cv.rectangle(img_copy, (x, y), (x+w, y+h), (0, 0, 0), 2)
-
+        hits = len(faces)
         log += str(hits) + " faces detected ...\n"
-        if hits > 0:
+
+        if hits > 0 and option == "detection":
+
+            for (x, y, w, h) in faces:
+                img_copy = cv.rectangle(img_copy, (x, y), (x+w, y+h),
+                                        (0, 0, 0), 2)
+
             name = layer.name + " faces"
             createNewLayer(img, name, img_copy)
             log += img.name + " create layer " + name + " ...\n"
 
-            # exportação via OpenCV (opcional)
-            img_out = cv.cvtColor(img_copy, cv.COLOR_BGR2RGB)
-            cv.imwrite("temp_" + layer.name, img_out)
+            pdb.gimp_selection_none(img)
+
+            # exportação via OpenCV (opcional - teste)
+            # img_out = cv.cvtColor(img_copy, cv.COLOR_BGR2RGB)
+            # cv.imwrite("temp_" + layer.name, img_out)
+
+        elif hits > 0 and option == "selection":
+
+            pdb.gimp_selection_none(img)
+            for (x, y, w, h) in faces:
+                pdb.gimp_image_select_ellipse(img, 0, x, y, w, h)
+                # pdb.gimp_image_select_rectangle(img, 0, x, y , w, h)
 
         else:
-            message("detection not possible.")
 
-        pdb.gimp_selection_none(img)
+            message("Detection not possible.")
+
         pdb.gimp_image_undo_group_end(img)
-
         gimp.displays_flush()
 
         log += inform + " ok\n"
@@ -174,6 +197,9 @@ register(
     [   # parâmetros de entrada do método
         (PF_IMAGE, "img", _("_Image"), None),
         (PF_DRAWABLE, "drw", _("_Drawable"), None),
+        (PF_RADIO, "option", "", "detection",
+          (("Only mark with a rectangular border.", "detection"),
+           ("Only select for later copy and paste.", "selection"))),
     ],
     [], # parâmetros de saída do método
     faceDetection,         # nome de chamada do método
