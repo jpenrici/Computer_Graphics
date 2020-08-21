ad63db95de8fc87f0d51f10315858794985cbd54
jpenrici <jpenrici@gmail.com>
2020-08-21 18:10:34 -0300

    Continuam os treinos com Numpy, array e classes py. 	modified:   NumPy_Training/img_numpy_training_1_draw.py

--- NumPy_Training/img_numpy_training_1_draw.py
+++ NumPy_Training/img_numpy_training_1_draw.py
@@ -2,10 +2,58 @@
 
 import numpy as np
 from PIL import Image
-from math import sqrt
+from math import sqrt, sin, cos
 
 
-class Img(object):
+class RGB:
+
+    __rgb = []
+
+    def __init__(self, red=255, green=255, blue=255):
+
+        self.__rgb = self.__config(red, green, blue)
+
+    def __config(self, red, green, blue):
+
+        # Limites
+        red = 0 if red < 0 else red
+        green = 0 if green < 0 else green
+        blue = 0 if blue < 0 else blue
+
+        red = 255 if red > 255 else red
+        green = 255 if green > 255 else green
+        blue = 255 if blue > 255 else blue
+
+        return [red, green, blue]
+
+    def setRGB(self, red, green, blue):
+
+        self.__rgb = self.__config(red, green, blue)
+
+    def setColor(self, rgb=None):
+
+        if rgb is None:
+            return
+        if len(rgb) != 3:
+            return
+
+        self.__rgb = self.__config(rgb[0], rgb[1], rgb[2])
+
+    def getRGB(self):
+
+        return self.__rgb
+
+
+class Color:
+
+    RED = [255, 0, 0]
+    GREEN = [0, 255, 0]
+    BLUE = [0, 0, 255]
+    BLACK = [0, 0, 0]
+    WHITE = [255, 255, 255]
+
+
+class Img:
 
     def __init__(self, height, width):
 
@@ -13,7 +61,7 @@ class Img(object):
         self.height = height
         self.rgbArray = np.zeros(height * width * 3, dtype='uint8')
 
-    def isValid(self, x, y):
+    def __isValid(self, x, y):
 
         if (x < 0 or x >= self.width):
             return False
@@ -22,9 +70,9 @@ class Img(object):
 
         return True
 
-    def setPixel(self, x, y, rgb=[0, 0, 0]):
+    def setPixel(self, x, y, rgb=Color.WHITE):
 
-        if (not self.isValid(x, y)):
+        if (not self.__isValid(x, y)):
             return
 
         pos = 3 * (x + y * self.width)
@@ -32,7 +80,7 @@ class Img(object):
         self.rgbArray[pos + 1] = rgb[1]  # Green
         self.rgbArray[pos + 2] = rgb[2]  # Blue
 
-    def line(self, x0, y0, x1, y1, rgb=[0, 0, 0]):
+    def line(self, x0, y0, x1, y1, rgb=Color.WHITE):
 
         x = x1 - x0
         y = y1 - y0
@@ -42,6 +90,12 @@ class Img(object):
         d = int(sqrt(x**2 + y**2))
         for i in range(d):
             self.setPixel(x0 + (i * step_x), y0 + (i * step_y), rgb)
+
+    def circle(self, x0, y0, radius=2, start=0, stop=360, rgb=Color.WHITE):
+        pass
+
+    def rectangle(self, x0, y0, height=2, width=2, rgb=Color.WHITE):
+        pass
 
     def view(self):
 
@@ -54,10 +108,21 @@ class Img(object):
 
 def test():
 
+    # RGB
+    c1 = RGB()
+    print(c1.getRGB())  # esperado [255, 255, 255]
+    c1.setRGB(-1, 0, 300)
+    print(c1.getRGB())  # esperado [0, 0, 255]
+    c1.setColor(Color.RED)
+    print(c1.getRGB())  # esperado [255, 0, 0]
+
+    # Desenhos
     a = Img(255, 255)
-    a.line(0, 0, 254, 254, [0, 0, 255])
-    a.line(127, 0, 127, 255, [255, 255, 255])
-    a.line(0, 254, 254, 0, [255, 0, 0])
+    a.line(0, 0, 254, 254)  # branca
+    a.line(127, 0, 127, 254, Color.BLUE)
+    a.line(0, 254, 254, 0, Color.GREEN)
+    a.line(0, 50, 254, 50, [127, 127, 127])
+    a.line(50, 127, 200, 127, c1.getRGB())
     a.view()
 
 
