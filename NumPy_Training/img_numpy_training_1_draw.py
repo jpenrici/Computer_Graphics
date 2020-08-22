a2d2901d9acb21285ea16a0480a79560cc4977de
jpenrici <jpenrici@gmail.com>
2020-08-21 21:36:16 -0300

    Treinar, treinar, treinar... 	new file:   NumPy_Training/images/draw_1.png

--- NumPy_Training/img_numpy_training_1_draw.py
+++ NumPy_Training/img_numpy_training_1_draw.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from PIL import Image
-from math import sqrt, sin, cos
+from math import sqrt, sin, cos, radians
 
 
 class RGB:
@@ -15,11 +15,12 @@ class RGB:
 
     def __config(self, red, green, blue):
 
-        # Limites
+        # Limite inferior
         red = 0 if red < 0 else red
         green = 0 if green < 0 else green
         blue = 0 if blue < 0 else blue
 
+        # Limite superior
         red = 255 if red > 255 else red
         green = 255 if green > 255 else green
         blue = 255 if blue > 255 else blue
@@ -46,6 +47,7 @@ class RGB:
 
 class Color:
 
+    # Cores pré definidas
     RED = [255, 0, 0]
     GREEN = [0, 255, 0]
     BLUE = [0, 0, 255]
@@ -55,11 +57,11 @@ class Color:
 
 class Img:
 
-    def __init__(self, height, width):
+    def __init__(self, height, width, background=Color.BLACK):
 
         self.width = width
         self.height = height
-        self.rgbArray = np.zeros(height * width * 3, dtype='uint8')
+        self.rgbArray = np.array(background * height * width, dtype='uint8')
 
     def __isValid(self, x, y):
 
@@ -72,13 +74,27 @@ class Img:
 
     def setPixel(self, x, y, rgb=Color.WHITE):
 
+        x = int(x)
+        y = int(y)
+
         if (not self.__isValid(x, y)):
+            # Não desenhar fora dos limites
             return
 
         pos = 3 * (x + y * self.width)
         self.rgbArray[pos + 0] = rgb[0]  # Red
         self.rgbArray[pos + 1] = rgb[1]  # Green
         self.rgbArray[pos + 2] = rgb[2]  # Blue
+
+    def getPixel(self, x, y):
+
+        if (not self.__isValid(x, y)):
+            return [None, None, None]
+
+        pos = 3 * (x + y * self.width)
+        pixel = self.rgbArray[pos:pos+3]
+
+        return pixel
 
     def line(self, x0, y0, x1, y1, rgb=Color.WHITE):
 
@@ -92,18 +108,35 @@ class Img:
             self.setPixel(x0 + (i * step_x), y0 + (i * step_y), rgb)
 
     def circle(self, x0, y0, radius=2, start=0, stop=360, rgb=Color.WHITE):
-        pass
+
+        start = 0 if (start < 0 or start > 360) else start
+        stop = 360 if (stop < 0 or stop > 360) else stop
+
+        for angle in range(start, stop):
+            x = x0 + int(radius * sin(radians(angle)))
+            y = y0 + int(radius * cos(radians(angle)))
+            self.setPixel(x, y, rgb)
 
     def rectangle(self, x0, y0, height=2, width=2, rgb=Color.WHITE):
-        pass
+
+        self.line(x0, y0, x0 + width, y0, rgb)
+        self.line(x0, y0, x0, y0 + height, rgb)
+        self.line(x0 + width, y0, x0 + width, y0 + height, rgb)
+        self.line(x0, y0 + height, x0 + width, y0 + height, rgb)
 
     def view(self):
 
         output = self.rgbArray.reshape(self.height, self.width, 3)
-        print(output)
+        # print(output)
 
         display = Image.fromarray(output, 'RGB')
         display.show()
+
+    def export(self, filename):
+
+        output = self.rgbArray.reshape(self.height, self.width, 3)
+        img = Image.fromarray(output, 'RGB')
+        img.save(filename)
 
 
 def test():
@@ -116,14 +149,40 @@ def test():
     c1.setColor(Color.RED)
     print(c1.getRGB())  # esperado [255, 0, 0]
 
-    # Desenhos
-    a = Img(255, 255)
-    a.line(0, 0, 254, 254)  # branca
-    a.line(127, 0, 127, 254, Color.BLUE)
-    a.line(0, 254, 254, 0, Color.GREEN)
-    a.line(0, 50, 254, 50, [127, 127, 127])
-    a.line(50, 127, 200, 127, c1.getRGB())
+    # Desenho
+    height = 300
+    width = 300
+
+    a = Img(height, width)
+    x = width - 1
+    y = height - 1
+
+    a.line(0, 0, x, y)
+    a.line(x/2, 0, x/2, y, Color.BLUE)
+    a.line(0, y, x, 0, Color.GREEN)
+    a.line(0, y/4, x, y/4, [127, 127, 127])
+    a.line(x/4, y/2, x/2, y/2, c1.getRGB())
+
+    a.circle(x/2, y/2, 25)
+    a.circle(x/2, y/2, 50, 90, 180, Color.RED)
+    a.circle(x/2, y/2, 75, 180, 270, Color.GREEN)
+    a.circle(x/2, y/2, 100, 270, 360, Color.BLUE)
+
+    a.rectangle(100, 100, 20, 20)
+    a.rectangle(150, 150, 40, 40, Color.GREEN)
+
     a.view()
+
+    print(a.getPixel(x*2, y*2))  # esperado [None, None, None]
+    print(a.getPixel(127, 0))    # esperado [0, 0, 255]
+
+    # Troca de background
+    for i in range(0, height):
+        for j in range(0, width):
+            if (a.getPixel(i, j) == Color.BLACK).all():
+                a.setPixel(i, j, [255, 127, 0])
+
+    a.export("./images/draw_1.png")  # esperado fundo colorido
 
 
 if __name__ == '__main__':
