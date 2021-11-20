49544e3c9f691e4ef75a6252b1e443623f1ba5b7
Juan Pablo Enrici <jpenrici@gmail.com>
2021-11-20 18:31:46 -0300

    Add files via upload

---
+++ OpenCV_Cpp_Training/HelloWorld/README.md
@@ -0,0 +1,23 @@
+# helloWorld
+
+Tested on Debian 9.
+
+# Requirements
+
+    cmake
+    opencv library
+
+# Run 
+
+	On the terminal execute the commands:
+		cmake CMakeLists.txt
+		make 
+		./helloWorld
+
+	Or:
+		chmod +x run
+		./run helloWorld
+		
+# Reference source
+
+	https://docs.opencv.org	
---
+++ OpenCV_Cpp_Training/Threshold/threshold_basic.cpp
@@ -0,0 +1,48 @@
+/* OpenCV 4.5.1 */
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    String imageName = "../../../Images/openclipart_tree-17.png";
+    if (argc > 1) {
+        imageName = argv[1];
+    }
+
+    Mat image = imread(imageName, IMREAD_COLOR);
+
+    if (image.empty()) {
+        std::cout << "Cannot read image: " << imageName << "\n";
+        return -1;
+    }
+
+    // Grayscale matrix
+    Mat grayscale_image(image.size(), CV_8U);
+
+    // Convert BGR to Gray
+    cvtColor(image, grayscale_image, COLOR_BGR2GRAY);
+
+    // Binary image
+    Mat binary_image(grayscale_image.size(), grayscale_image.type());
+
+    // Apply thresholding
+    threshold(grayscale_image, binary_image, 1, 255, THRESH_BINARY);
+
+    // Show the original
+    namedWindow("Original", WINDOW_AUTOSIZE);
+    imshow("Original", image);
+
+    // Show the results
+    namedWindow("Output", WINDOW_AUTOSIZE);
+    imshow("Output", binary_image);
+
+    // Save automatically
+    // imwrite("Output.jpg", binary_image);
+    // imwrite("Output.png", binary_image);
+
+    waitKey();
+
+    return 0;
+}
